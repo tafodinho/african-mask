ac42353b52ccc6d66b551456921f14caa703a2b7
spycode1 <tafodinho@gmail.com>
2022-03-14 18:29:06 +0100

    first commit

---
+++ client/build/precache-manifest.ce22becfa9dbb01f50b2765596e4c59e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2eeea667b8564ac3958302e5a0715727",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9469c6e6b1beee1cee3",
+    "url": "/static/css/2.25dbc16d.chunk.css"
+  },
+  {
+    "revision": "4b612e7386a2fef5c2ac",
+    "url": "/static/css/main.64bbee64.chunk.css"
+  },
+  {
+    "revision": "b9469c6e6b1beee1cee3",
+    "url": "/static/js/2.fe9f32e4.chunk.js"
+  },
+  {
+    "revision": "4b612e7386a2fef5c2ac",
+    "url": "/static/js/main.1bc0b779.chunk.js"
+  },
+  {
+    "revision": "0a31ddf7382e0363a369",
+    "url": "/static/js/runtime-main.b240f91f.js"
+  },
+  {
+    "revision": "a51302e308a36f0b580e973e4dd26d62",
+    "url": "/static/media/2.a51302e3.png"
+  },
+  {
+    "revision": "96a77b983e62d9fe0cfdff875e4971d1",
+    "url": "/static/media/5.96a77b98.png"
+  },
+  {
+    "revision": "3fd9f3315f9adc2039a2bc02c2bf0a1d",
+    "url": "/static/media/6.3fd9f331.png"
+  },
+  {
+    "revision": "c57ce054ba17f745c3b2516a21057310",
+    "url": "/static/media/background2.c57ce054.jpeg"
+  },
+  {
+    "revision": "44b89253a7fdbd8a27ba74559f34659e",
+    "url": "/static/media/bid-image1.44b89253.png"
+  }
+]);
